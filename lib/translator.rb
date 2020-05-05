609481ca2a149d5def36bb062219096535923419
Andrew Hawley <ahawley785@gmail.com>
2020-05-05 17:30:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ end
 
 def get_japanese_emoticon(file_path,eng_emoticon)
   updated_library = load_library(file_path)
+  
   updated_library.each do  |name_of, values|
     #puts name_of
     #puts values
@@ -23,8 +24,7 @@ def get_japanese_emoticon(file_path,eng_emoticon)
       #puts language
       #puts elements
       if elements == eng_emoticon
-        puts language
-
+        puts name_of
       end
 
     end
