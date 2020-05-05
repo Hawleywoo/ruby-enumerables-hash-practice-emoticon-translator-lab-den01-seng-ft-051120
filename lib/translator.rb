3561693ce47a0799b3a2e080cb92730d2bbc17e3
Andrew Hawley <ahawley785@gmail.com>
2020-05-05 16:50:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,7 +30,7 @@ def get_english_meaning(file_path, jap_emoticon)
       #puts language
       #puts elements
       if elements == jap_emoticon
-        puts elements
+        puts name_of
 
       end
 
