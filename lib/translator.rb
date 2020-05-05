ee10f224173dc8cfcab88878a8fabe75fc1b5373
Andrew Hawley <ahawley785@gmail.com>
2020-05-05 16:28:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  updated_library = {}
+  library_hash = YAML.load_file(file_path)
+  library_hash.each do |key, values|
+    updated_library[key] = {
+      :english => library_hash[key][0],
+      :japanese => library_hash[key][1]}
+  end
+  updated_library
 end
 
 def get_japanese_emoticon
   # code goes here
+
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, jap_emoticon)
   # code goes here
+  updated_library = load_library(file_path)
+  #p updated_library['wink']
+  updated_library.each do |name|
+    name.each do |language, emoticon|
+      p Language
+      p emoticon
+    end
+
+    #p item[:english].class
+    #p item[:japanese].class
+    #p name[item][:english]
+  end
 end
+get_english_meaning('./lib/emoticons.yml',"(＾ｖ＾)")
